106d26c69ac2687f06bba9801d07049b088db2a4
Jarek Radosz <jarek@cvx.dev>
2025-10-08 21:46:06 +0200

    DEV: Update linting

--- javascripts/discourse/api-initializers/post-quote-copy-button.js
+++ javascripts/discourse/api-initializers/post-quote-copy-button.js
@@ -1,6 +1,6 @@
 import { apiInitializer } from "discourse/lib/api";
 import PostCopyQuoteButton from "../components/post-copy-quote-button";
 
-export default apiInitializer("0.11.1", (api) => {
+export default apiInitializer((api) => {
   api.renderInOutlet("quote-share-buttons-after", PostCopyQuoteButton);
 });
